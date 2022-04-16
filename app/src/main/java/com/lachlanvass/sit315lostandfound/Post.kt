6739cacd6f2a34a3b61cb63b45fbca066226eec8
lachlan-vass <lachlan.vass@superretailgroup.com>
2022-04-16 17:07:15 +1000

    add room data class and DAO

---
+++ app/src/main/java/com/lachlanvass/sit315lostandfound/Post.kt
@@ -0,0 +1,24 @@
+package com.lachlanvass.sit315lostandfound
+
+import androidx.room.*
+
+@Entity
+data class Post(
+
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "phone_number") val phoneNumber: String?,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "date") val date: String?,
+    @ColumnInfo(name = "location") val location: String?
+)
+
+@Dao
+interface PostDao {
+
+    @Query("SELECT * FROM Post")
+    fun getAll(): List<Post>
+
+    @Delete
+    fun delete(post: Post)
+}
