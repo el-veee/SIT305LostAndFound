79454a75e1218de0c0bf99b01414e63154ee9adf
lachlan-vass <lachlan.vass@superretailgroup.com>
2022-04-16 14:58:23 +1000

    add extra activities + logic for switching

---
+++ app/src/main/java/com/lachlanvass/sit315lostandfound/CreateLostAndFoundActivity.kt
@@ -0,0 +1,11 @@
+package com.lachlanvass.sit315lostandfound
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateLostAndFoundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_lost_and_found)
+    }
+}
--- app/src/main/java/com/lachlanvass/sit315lostandfound/MainActivity.kt
+++ app/src/main/java/com/lachlanvass/sit315lostandfound/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.lachlanvass.sit315lostandfound
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val createLostAndFoundButton = findViewById<Button>(R.id.create_advert_button)
+        val showAllButton = findViewById<Button>(R.id.show_all_button)
+
+        val buttonIntentMap = mapOf<Button, Intent>(
+
+            createLostAndFoundButton to Intent(this, CreateLostAndFoundActivity::class.java),
+            showAllButton to Intent(this, ListLostAndFoundActivity::class.java)
+        )
+
+        buttonIntentMap.entries.forEach {(button, intent) ->
+
+            button.setOnClickListener {
+                startActivity(intent)
+            }
+        }
     }
 }
